d08ab46faa559d6e921ee96dc147f020e44a72e4
vian13ab <vilde.b.a@gmail.com>
2014-11-06 14:32:21 +0100

    Save note objects

--- ServerTEO/src/SaveNoteObject.java
+++ ServerTEO/src/SaveNoteObject.java
@@ -1,4 +1,30 @@
 
 public class SaveNoteObject {
+	
+	private String noteAuthor;
+	private String noteContent;
+	private String noteEvent;
+	
+	
+	public String getNoteAuthor() {
+		return noteAuthor;
+	}
+	public void setNoteAuthor(String noteAuthor) {
+		this.noteAuthor = noteAuthor;
+	}
+	public String getNoteContent() {
+		return noteContent;
+	}
+	public void setNoteContent(String noteContent) {
+		this.noteContent = noteContent;
+	}
+	public String getNoteEvent() {
+		return noteEvent;
+	}
+	public void setNoteEvent(String noteEvent) {
+		this.noteEvent = noteEvent;
+	}
+	
+	
 
 }
