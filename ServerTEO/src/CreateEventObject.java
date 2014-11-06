34f6c9c4d5f08c4641a891c54b2a84ce5d6f46b0
vian13ab <vilde.b.a@gmail.com>
2014-11-06 15:12:44 +0100

    create event object

--- ServerTEO/src/CreateEventObject.java
+++ ServerTEO/src/CreateEventObject.java
@@ -1,4 +1,51 @@
 
-public class CreateEventObject {
+public class CreateEventObject implements java.io.Serializable
 
+{
+	private static final long serialVersionUID = 603624733895783558L;
+	private String overallID = "deleteEvent";
+	public String name;
+	public String location;
+	public String timeStart;
+	public String timeFinished;
+	public String author;
+	
+	public String getOverallID() {
+		return overallID;
+	}
+	public void setOverallID(String overallID) {
+		this.overallID = overallID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getTimeStart() {
+		return timeStart;
+	}
+	public void setTimeStart(String timeStart) {
+		this.timeStart = timeStart;
+	}
+	public String getTimeFinished() {
+		return timeFinished;
+	}
+	public void setTimeFinished(String timeFinished) {
+		this.timeFinished = timeFinished;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	
 }
