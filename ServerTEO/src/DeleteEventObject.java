b1e07bc3e4c5558cb697153ad920fcec05dffe07
vian13ab <vilde.b.a@gmail.com>
2014-11-06 14:14:54 +0100

    Delete events objects

--- ServerTEO/src/DeleteEventObject.java
+++ ServerTEO/src/DeleteEventObject.java
@@ -1,4 +1,21 @@
 
 public class DeleteEventObject {
+	
+	private String eventToDelete;
+	private String authEvent;
+	
+	
+	public String getEventToDelete() {
+		return eventToDelete;
+	}
+	public void setEventToDelete(String eventToDelete) {
+		this.eventToDelete = eventToDelete;
+	}
+	public String getAuthEvent() {
+		return authEvent;
+	}
+	public void setAuthEvent(String authEvent) {
+		this.authEvent = authEvent;
+	}
 
 }
