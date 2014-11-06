d9792219083065d49d138b2f55135e6f7cb01f02
hognipetersen <hognipetersen@gmail.com>
2014-11-06 15:43:57 +0100

    Gson libraries importeret og lavet Build Path

--- ServerTEO/src/DeleteEventObject.java
+++ ServerTEO/src/DeleteEventObject.java
@@ -1,13 +1,10 @@
 
-public class DeleteEventObject implements java.io.Serializable
-{
+public class DeleteEventObject implements java.io.Serializable{
 	
 	private static final long serialVersionUID = -4749520182118516370L;
 	private String overallID = "deleteEvent";
 	private String eventToDelete;
 	private String authEvent;
-	
-	
 	
 	public String getOverallID() {
 		return overallID;
