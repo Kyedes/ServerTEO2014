1bf328246453936bc24fdc4b87efabdae10761d2
hognipetersen <hognipetersen@gmail.com>
2014-11-06 18:37:01 +0100

    LoginAuth.java

---
+++ ServerTEO/src/LogInAuth.java
@@ -0,0 +1,9 @@
+// HHP: Denne klasse skal indeholde en login successfull true/false boolean og en switch som kan ændre denne boolean alt 
+// efter hvad LogIn.java klassen returnerer af 5 forskellige variabler.
+public class LogInAuth {
+
+	private boolean LogInSuccessful = false;
+	
+	
+	
+}
