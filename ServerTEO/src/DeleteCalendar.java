df11397d8da9bfad7f10611ff4fb64b3049af67f
Kyedes <esky13ab@student.cbs.dk>
2014-11-09 10:56:42 +0100

    DeleteCalendar funktion oprettet, men mangler at slette relaterede events.

--- ServerTEO/src/DeleteCalendar.java
+++ ServerTEO/src/DeleteCalendar.java
@@ -1,4 +1,42 @@
+import java.sql.SQLException;
 
-public class DeleteCalendar {
+import model.Model;
+import model.QueryBuild.QueryBuilder;
+
+public class DeleteCalendar extends Model{
+	private DatabaseTableConfigurations dbConfig = new DatabaseTableConfigurations();
+	private QueryBuilder queryBuilder = new QueryBuilder();
+	
+	public String execute(DeleteCalendarObject deleteCalendarObject) throws SQLException{
+		String answer = "";
+		
+		resultSet = queryBuilder.selectFrom(dbConfig.getCalendar()).where("Name", "=", deleteCalendarObject.getCalendarToDelete()).ExecuteQuery();
+		
+		while(resultSet.next()){
+			String [] value = {"CreatedBy"};
+			
+			//TODO compare list of user/calendar with user and calendar name
+			
+			String calendarAuthor = queryBuilder.selectFrom(value, dbConfig.getCalendar()).where("Name", "=", deleteCalendarObject.getCalendarToDelete()).ExecuteQuery().toString();
+			
+			if(deleteCalendarObject.getAuthCalendar().equalsIgnoreCase(calendarAuthor)){
+				
+				
+				
+				queryBuilder.deleteFrom(dbConfig.getCalendar()).where("Name", "=", deleteCalendarObject.getCalendarToDelete());
+							
+				//TODO Create code to delete events associated with calendar
+				
+				answer = String.format("Calendar " + deleteCalendarObject.getCalendarToDelete() + "has been deleted.");
+				
+			}else{
+				answer = "Your are not the owner of this calendar";
+				
+			}
+		}
+		
+		
+		return answer;
+	}
 
 }
