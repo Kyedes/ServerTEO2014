6d94e4e45c9b4f2a90b411a31ffcb51b097b4ed0
hognipetersen <hognipetersen@gmail.com>
2014-11-10 10:52:28 +0100

    SaveNote

--- ServerTEO/src/SaveNote.java
+++ ServerTEO/src/SaveNote.java
@@ -1,4 +1,23 @@
+import java.sql.SQLException;
 
-public class SaveNote {
+import model.Model;
+import model.QueryBuild.QueryBuilder;
+
+public class SaveNote extends Model{
+	private DatabaseTableConfigurations dbConfig = new DatabaseTableConfigurations();
+	private QueryBuilder queryBuilder = new QueryBuilder();
+	
+	public String execute(SaveNoteObject saveNoteObject) throws SQLException{
+		String answer = "";
+		
+		String [] valueCalendar = {"calendarID"};
+		String [] valueEvent = {"eventID"};
+		String [] valueNote = {"noteID"};
+		String [] valueUser = {"userID"};
+		
+		String noteID = queryBuilder.selectFrom(valueNote, "notes").where("NoteName", "=", saveNoteObject.getNoteEvent()).ExecuteQuery().toString();
+		
+		int userID = queryBuilder.selectFrom(valueUser, "Users").where("UserName", "=", saveNoteObject.getNoteEvent()).ExecuteQuery().getInt(1);
+	}
 
 }
